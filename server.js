8fa1c7d6364b7ef358cbe19384e2a42bacafae41
terrylowery <terry@lowerytech.net>
2018-02-26 23:46:00 -0600

    Added support for partials and added handlers for getyear and screamit

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    text = text + '!';
+    return text.toUpperCase();
+})
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello express</h1>');
