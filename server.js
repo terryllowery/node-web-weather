0c6db1082ac4250e0ea84d9f8e7e5b0e4b22b711
terrylowery <terry@lowerytech.net>
2018-02-27 00:21:19 -0600

    Updated .gitignore and server.js

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: Hostname ${req.hostname}: SourceIp ${req.ip}: Method ${req.method}: Url ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log('Unable to append to server log' + err);
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
+
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
